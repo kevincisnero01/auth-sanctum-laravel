b4ecf412c9e5a27dc7ebaf45e822414076e7352a
kevincisnero01 <7884578+kevincisnero01@users.noreply.github.com>
2022-11-09 23:37:49 -0400

    User Controller | Register

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json([
+            'status' => 1,
+            'message' =>  'Registro Exitoso'
+        ]);
+        
+    }
+}
